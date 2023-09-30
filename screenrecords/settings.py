1bfcf8f227c82f8e4dc51d4195725e00cafc210f
kurves <kavssimply@gmail.com>
2023-09-30 14:01:53 +0300

    add dependencies

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import Config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-config=Config(RepositoryEnv(".env"))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
