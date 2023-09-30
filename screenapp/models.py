7d1f89d4c8dbdb4a7949d135508d8c31e3fd1f7b
kurves <kavssimply@gmail.com>
2023-09-30 10:57:22 +0300

    Create transcription for video

--- screenapp/models.py
+++ screenapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import os
+import subprocess
+import whisper
 
 # Create your models here.
 
@@ -7,5 +10,36 @@ class ScreenVideo(models.Model):
     title=models.CharField(max_length=100)
     video_file=models.FileField(upload_to="recorded_videos/")
     timestamp=models.DateTimeField(auto_now_add=True)
+    transcription=models.TextField(blank=True,null=True)
+
+
+    def trancribe_audio(self,audio_path):
+        response = openai.Transcription.create(
+            audio=openai.Audio.from_file(audio_path, content_type="audio/wav"),
+            model="whisper-large",)
+        
+        transcription_result = response['text']
+        return transcription_result
+
+
+    def transcribe_video(self):
+        video_path=self.video_file.video_path
+
+        audio_path=os.path.join('media','temp_audio.wav')
+        transcription_path=os.path.join('media','transcription.txt')
+
+        subprocess.run(['ffmpeg','-i',video_path,'-vn','-acodec','pcm_s161e','-ar','44100','-ac','2',audio_path])
+
+        api_key = openai_secret_manager.get_secret("api_whisper_key") 
+        openai.api_key = api_key["api_key"]
+
+        transcription_result = trancribe_audio(audio_path)
+
+        with open(transcription_path, 'w') as file:
+            file.write(transcription_result)
+        self.transcription = transcription_result
+        self.save()    
+    def __str__(self):
+        return self.title 
 
 
