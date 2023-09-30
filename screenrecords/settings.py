14c3c923c4d6fe44a5ae1d9efcac8bbcc6ef17fb
kurves <kavssimply@gmail.com>
2023-09-30 13:48:25 +0300

    Env settings

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -12,18 +12,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+config=Config(RepositoryEnv(".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-t$ihhj$#29%jm$u9rdgdg$&-@mx6d8ke1%%e58t7n*uxpew+)9'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
