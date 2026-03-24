5631da6fa1b0b4874323038b457c4aa5c0ecfdc0
kurves <kavssimply@gmail.com>
2026-03-24 22:16:43 +0300

    remove sensitive app info

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -21,12 +21,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY',)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-t$ihhj$#29%jm$u9rdgdg$&-@mx6d8ke1%%e58t7n*uxpew+)9'
-SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
